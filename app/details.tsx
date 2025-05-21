f4d55e98eabf8444f42b49740379dd478894c357
Guilherme <guilherme.brunner@gmail.com>
2025-05-21 20:50:59 -0300

    feat: adds ImageDetails implementation to details route

--- app/details.tsx
+++ app/details.tsx
@@ -1,16 +1,27 @@
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { useLocalSearchParams } from 'expo-router';
+import { IMAGES } from '../constants/Images';
+import { StyleSheet, ImageBackground } from 'react-native';
 
-export default function Details() {
+type ImageDetailsProps = {
+  /** Id of the displayed image */
+  id: string;
+};
+
+export default function ImageDetails() {
+  const { id } = useLocalSearchParams<'/details', ImageDetailsProps>();
+
   return (
-    <ThemedView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <ThemedText>This is the image details screen</ThemedText>
-    </ThemedView>
+    <ImageBackground
+      style={styles.image}
+      source={{ uri: IMAGES.find((image) => image.id === id)?.download_url }}
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
