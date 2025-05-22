500737d30743520de749718eaec5f461789d6b44
Guilherme <guilherme.brunner@gmail.com>
2025-05-21 22:19:02 -0300

    feat: adds GalleryImageItem component

---
+++ components/GalleryImageItem.tsx
@@ -0,0 +1,29 @@
+import { ImageProps } from '@/types';
+import { Link } from 'expo-router';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+interface GalleryImageItemProps {
+  id: ImageProps['id'];
+  uri: ImageProps['download_url'];
+}
+
+export function GalleryImageItem({ id, uri }: GalleryImageItemProps) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <Link href={{ pathname: '/details', params: { id } }}>
+        <Image source={{ uri }} style={styles.image} />
+      </Link>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+  image: {
+    width: 200,
+    height: 300,
+    borderRadius: 8,
+  },
+});
